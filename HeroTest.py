3ed92328e0563fe68ece6e2d52c12ae9bce55b03
Ivan <ivannborisov@gmail.com>
2015-04-17 00:11:50 +0300

    Methods take_damage,take_healing,take_mana were created

--- HeroTest.py
+++ HeroTest.py
@@ -34,6 +34,20 @@ class HeroTest(unittest.TestCase):
         self.test_hero.learn(s)
         self.assertFalse(self.test_hero.can_cast())
 
+    def test_take_damage(self):
+        self.test_hero.take_damage(35)
+        self.assertEqual(65, self.test_hero.get_health())
+
+    def test_take_healing(self):
+        self.test_hero.take_damage(60)
+        self.test_hero.take_healing(35)
+        self.assertEqual(75, self.test_hero.get_health())
+
+    def test_take_mana(self):
+        self.test_hero.set_mana(30)
+        self.test_hero.take_mana(mana_points=50)
+        self.assertEqual(80, self.test_hero.get_mana())
+
 
 if __name__ == '__main__':
     unittest.main()
